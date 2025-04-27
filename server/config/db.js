e1c53d3ab0f530095f4839ef2649f1c290d0a802
abhay <abhaydeep7890@gmail.com>
2025-04-27 21:34:47 +0530

    mongo atlas configured

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/petvantageTest")
+// mongoose.connect("mongodb://localhost:27017/petvantageTest")
+mongoose.connect("mongodb+srv://abhaysinghdeep7890:HrXBVZxPu3NPiO0p@cluster0.li8zajr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
     console.log("Database Connected Succesfuly!");
     
